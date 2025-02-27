1de8bf6b3c9ff4c1061d96c0741c5fa8297c878d
MohakGupta2004 <mohakgupta500@gmail.com>
2025-02-28 02:25:24 +0530

    feat: add links to prod

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/v1', // Set your base URL here
+  // baseURL: 'http://localhost:3000/api/v1', // Set your base URL here
+  baseURL: 'https://work3-y5ld.onrender.com'
 });
 
 export default api; 

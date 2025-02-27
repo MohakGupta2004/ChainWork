8651666d2dc487c909dc0cd89931976c036040ff
MohakGupta2004 <mohakgupta500@gmail.com>
2025-02-28 02:30:01 +0530

    fix: api.js

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:3000/api/v1', // Set your base URL here
-  baseURL: 'https://work3-y5ld.onrender.com'
+  baseURL: 'https://work3-y5ld.onrender.com/api/v1'
 });
 
 export default api; 

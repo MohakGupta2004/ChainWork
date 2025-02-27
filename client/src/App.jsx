844eb365f5e46e6f2d98842dd1c1720de331906e
Soumasish Dasgupta <dasguptasoumasish@gmail.com>
2025-02-27 19:20:29 +0530

    modified:   client/src/App.jsx 	modified:   client/src/pages/CreateProject.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,7 +22,7 @@ const Layout = () => {
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <Outlet />
       </main>
-      {/* <Footer/> */}
+      <Footer/>
     </div>
   );
 };
--- client/src/pages/CreateProject.jsx
+++ client/src/pages/CreateProject.jsx
@@ -32,41 +32,41 @@ export default function CreateProject() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex items-center justify-center h-full py-28 bg-gray-800/30 backdrop-blur-sm rounded-3xl border border-gray-700/50 hover:bg-gray-800/40 transition-all duration-300">
       <div className="w-1/2">
-        <h1 className="text-2xl font-bold mb-4 text-center">Create New Project</h1>
+        <h1 className="text-2xl font-bold mb-4 text-center text-orange-500">Create New Project</h1>
         <form onSubmit={handleCreateProject}>
           <div className="mb-4">
-            <label className="block text-gray-800 mb-2" htmlFor="title">Project Title</label>
+            <label className="block text-gray-300 mb-2 text-left" htmlFor="title">Project Title</label>
             <input
               type="text"
               id="title"
               value={title}
               onChange={(e) => setTitle(e.target.value)}
-              className="w-full p-2 border rounded"
+              className="w-full p-2 border rounded hover:border-2 hover:border-orange-500"
               placeholder="Enter project title"
               required
             />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-800 mb-2" htmlFor="description">Description</label>
+            <label className="block text-left text-gray-300 mb-2" htmlFor="description">Description</label>
             <textarea
               id="description"
               value={description}
               onChange={(e) => setDescription(e.target.value)}
-              className="w-full p-2 border rounded"
+              className="w-full p-2 border rounded hover:border-2 hover:border-orange-500"
               placeholder="Enter project description"
               required
             />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-800 mb-2" htmlFor="budget">Budget (ETH)</label>
+            <label className="block text-left text-gray-300 mb-2" htmlFor="budget">Budget (ETH)</label>
             <input
               type="number"
               id="budget"
               value={budget}
               onChange={(e) => setBudget(e.target.value)}
-              className="w-full p-2 border rounded"
+              className="w-full p-2 mb-8 border rounded hover:border-2 hover:border-orange-500"
               placeholder="Enter project budget"
               required
             />

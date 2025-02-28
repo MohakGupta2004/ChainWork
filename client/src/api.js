450934a379cbd6629b90d905affd021cf5ceb34f
MohakGupta2004 <mohakgupta500@gmail.com>
2025-02-28 07:54:09 +0530

    fix: api name change

--- client/src/api.js
+++ client/src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/v1', // Set your base URL here
-  // baseURL: 'https://work3-y5ld.onrender.com/api/v1'
+  // baseURL: 'http://localhost:3000/api/v1', // Set your base URL here
+  baseURL: 'https://work3-y5ld.onrender.com/api/v1'
 });
 
 export default api; 
